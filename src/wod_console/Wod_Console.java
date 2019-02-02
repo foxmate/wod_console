4074af802c7f2a257cd3584236d1278d778f424c
Przemek Gajda <foxmate@MBPPrzemekGajda.home>
2019-02-02 14:58:38 +0100

    dodanie zapisu do pliku

--- src/wod_console/Wod_Console.java
+++ src/wod_console/Wod_Console.java
@@ -2,20 +2,41 @@ package wod_console;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 
 public class Wod_Console {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		Document conn;
+		String fileName = "out.txt";
+		FileWriter plik = new FileWriter(fileName);
+		
 		try {
-			Document conn = Jsoup.connect("https://www.crossfit.com/workout/2017/06/10/").get();
-			String dane = conn.getElementsByClass("content").outerHtml();
-			System.out.println(dane);
+			String dane = "";
+			int i = 1;
+			do {
+				conn = Jsoup.connect("https://www.crossfit.com/workout?page=" + i).get();
+				dane = dane + conn.getElementsByClass("content").html();
+				plik.write(dane);
+				i++;
+				System.out.println("i: " + i);
+				//System.out.println(dane);
+			/*String tab[] = dane.split("<h3");
+			for (String r : tab) {
+				System.out.println(r);
+			}*/
+			}
+			while(
+				i < 3 //!conn.getElementsByClass("content").text().isEmpty()
+			);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Test");
+		plik.close();
+		System.out.println("Koniec");
 	}
 
 }
